dcffa00ea19969d0e01d7e1708817bfcb17aea65
Oleg Plakhotniuk <olegus8@gmail.com>
2015-03-28 18:47:03 -0500

    tag

--- www/run.sh
+++ www/run.sh
@@ -3,17 +3,15 @@
 set +e
 docker stop toothris-www
 docker rm toothris-www
-docker rmi toothris/toothris-www
 set -e
 
 mkdir -p /var/tmp/toothris-www
 chown 1000:1000 /var/tmp/toothris-www
 
-docker build --rm -t toothris/toothris-www /usr/share/toothris-www/www
-
 docker run --rm \
     -v /etc/localtime:/etc/localtime:ro \
     -v /etc/toothris-www:/etc/toothris-www:ro \
     -v /var/tmp/toothris-www:/var/tmp/toothris-www \
-    --name toothris-www -h toothris-www toothris/toothris-www \
+    --name toothris-www -h toothris-www \
+    toothris/toothris-www:0.0.0dev-2015-01-02 \
     sudo -iu user /toothris-www/run.sh
