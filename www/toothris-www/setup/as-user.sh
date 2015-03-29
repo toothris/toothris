8420fb7089c4ae0da71a6cf1178eb733a053dd86
Oleg Plakhotniuk <olegus8@gmail.com>
2015-03-28 20:02:13 -0500

    fix

--- www/toothris-www/setup/as-user.sh
+++ www/toothris-www/setup/as-user.sh
@@ -4,18 +4,6 @@ set -e
 
 . /toothris-www/setup/share.sh
 
-# libxmp
-fetch-aur /li libxmp
-makepkg -sc --noconfirm
-sudo pacman -U --noconfirm libxmp-4.3.2-1-x86_64.pkg.tar.xz
-rm libxmp-4.3.2-1-x86_64.pkg.tar.xz
-
-# xmp
-fetch-aur /xm xmp
-makepkg -sc --noconfirm
-sudo pacman -U --noconfirm xmp-4.0.10-1-x86_64.pkg.tar.xz
-rm xmp-4.0.10-1-x86_64.pkg.tar.xz
-
 # stackless-python2
 fetch-aur /st stackless-python2
 makepkg -sc --noconfirm
@@ -36,6 +24,18 @@ cd /toothris-www/setup/toothris
 makepkg -sc --noconfirm
 sudo pacman -U --noconfirm toothris-0.0.0dev-1-any.pkg.tar.xz
 rm toothris-0.0.0dev-1-any.pkg.tar.xz
+
+# libxmp
+fetch-aur /li libxmp
+makepkg -sc --noconfirm
+sudo pacman -U --noconfirm libxmp-4.3.2-1-x86_64.pkg.tar.xz
+rm libxmp-4.3.2-1-x86_64.pkg.tar.xz
+
+# xmp
+fetch-aur /xm xmp
+makepkg -sc --noconfirm
+sudo pacman -U --noconfirm xmp-4.0.10-1-x86_64.pkg.tar.xz
+rm xmp-4.0.10-1-x86_64.pkg.tar.xz
 
 # ffmpeg-libfdk_aac
 fetch-aur /ff ffmpeg-libfdk_aac
