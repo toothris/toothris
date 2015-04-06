1a40bff14c172e717ffd66fd6cf06cbea5a42cb5
Oleg Plakhotniuk <olegus8@gmail.com>
2015-04-06 17:53:24 -0500

    deploy

---
+++ www/toothris-www/run/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. /etc/toothris-www/config.sh
+
+export AWS_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY
--- www/toothris-www/run/run.sh
+++ www/toothris-www/run/run.sh
@@ -2,3 +2,4 @@
 set -e
 /toothris-www/run/makevideo.sh
 /toothris-www/run/makewww.sh
+/toothris-www/run/deploy.sh
