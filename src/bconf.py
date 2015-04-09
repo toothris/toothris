63b92203a80b0f7034ed741b5a3d5694028985d9
Oleg Plakhotniuk <olegus8@gmail.com>
2015-04-08 19:28:27 -0500

    whitespace

--- src/bconf.py
+++ src/bconf.py
@@ -18,26 +18,26 @@
 import optparse
 
 parser = optparse.OptionParser(prog='toothris', usage='%prog [options]',
-                               version='%prog 0.0.0dev')
+                               version='%prog 1.0.0')
 parser.add_option('', '--width', default=800, type='int', dest='width',
                   help='Drawing area width.')
 parser.add_option('', '--height', default=600, type='int', dest='height',
                   help='Drawing area height.')
 parser.add_option('', '--fullscreen', default=False, dest='fullscreen',
                   action='store_true', help='Fullscreen mode.')
-parser.add_option('', '--fps', default=60, type='int', dest='fps', 
+parser.add_option('', '--fps', default=60, type='int', dest='fps',
                   help='Target frames per second.')
-parser.add_option('', '--freefps', default=True, dest='keepfps', 
+parser.add_option('', '--freefps', default=True, dest='keepfps',
                   action='store_false', help='Don\'t maintain target FPS.')
-parser.add_option('', '--norender', default=True, dest='render', 
+parser.add_option('', '--norender', default=True, dest='render',
                   action='store_false', help='Don\'t draw anything.')
-parser.add_option('', '--replay', default=False, dest='replay', 
+parser.add_option('', '--replay', default=False, dest='replay',
                   action='store_true', help='Replay events from file.')
-parser.add_option('', '--record', default=False, dest='record', 
+parser.add_option('', '--record', default=False, dest='record',
                   action='store_true', help='Records events to file.')
-parser.add_option('', '--events', default='toothris.events', dest='events', 
+parser.add_option('', '--events', default='toothris.events', dest='events',
                   help='Events file name.')
-parser.add_option('', '--music', default='', dest='music', 
+parser.add_option('', '--music', default='', dest='music',
                   help='Music file name.')
 parser.add_option('', '--frames', default='', dest='frames',
                   help='Save each frame to file.')
